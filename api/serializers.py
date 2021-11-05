3992e0aded7f59d740c2fd46f7ae8338d7b66a9c
blatnoy7447 <cool.backstreet@gmail.com>
2021-11-05 22:31:15 +0500

    filterreview and recursive serializer have done

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,20 @@ from rest_framework import serializers
 from articles.models import Post, Review
 
 
+class FilterReviewListSerializer(serializers.ListSerializer):
+    """Фильтр комментариев, только parents"""
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveSerializer(serializers.Serializer):
+    """Вывод рекурсивно children"""
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
 class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -18,9 +32,12 @@ class ReviewCreateSerializer(serializers.ModelSerializer):
 
 class ReviewSerializer(serializers.ModelSerializer):
     """Вывод отзыва"""
+    children = RecursiveSerializer(many=True)
+
     class Meta:
+        list_serializer_class = FilterReviewListSerializer
         model = Review
-        fields = ('text', 'parent')
+        fields = ('text', 'children')
 
 
 class PostDetailSerializer(serializers.ModelSerializer):
@@ -30,6 +47,3 @@ class PostDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = "__all__"
-
-
-
