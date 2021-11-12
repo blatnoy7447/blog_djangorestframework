06f622880b3f66f84e7e2cb02a4ecddd14e17664
blatnoy7447 <cool.backstreet@gmail.com>
2021-11-12 08:25:40 +0500

    wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# project home directory to the sys.path
+project_home = '/home/blatnoy/blog_drf'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'blog_drf.settings'
+
+# server django via WSGI
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = get_wsgi_application()
