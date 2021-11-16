f61e870322b0eb4838028f103ffe0d1cf18140f4
marufyuldashev <62686688+blatnoy7447@users.noreply.github.com>
2021-11-16 19:36:13 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'postgres',
-        'PASSWORD': '251463',
+        'PASSWORD': 'pass',
         'HOST': 'localhost',
         'PORT': '5432',
     }
